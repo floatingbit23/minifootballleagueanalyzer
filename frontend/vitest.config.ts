e1ee789478fb31cd8b41c24b2b31ee0631747488
floatingbit23 <javisancab2003@gmail.com>
2026-06-01 17:21:13 +0200

    fix: Vitest conflict with Playwright

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -6,16 +6,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'jsonwebtoken': path.resolve(__dirname, '../node_modules/jsonwebtoken'),
-      'stripe': path.resolve(__dirname, '../node_modules/stripe'),
-      '@aws-sdk/client-dynamodb': path.resolve(__dirname, '../node_modules/@aws-sdk/client-dynamodb'),
-      '@aws-sdk/lib-dynamodb': path.resolve(__dirname, '../node_modules/@aws-sdk/lib-dynamodb'),
+      'jsonwebtoken': path.resolve(__dirname, './node_modules/jsonwebtoken'),
+      'stripe': path.resolve(__dirname, './node_modules/stripe'),
+      '@aws-sdk/client-dynamodb': path.resolve(__dirname, './node_modules/@aws-sdk/client-dynamodb'),
+      '@aws-sdk/lib-dynamodb': path.resolve(__dirname, './node_modules/@aws-sdk/lib-dynamodb'),
     }
   },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test-setup.ts'],
-    exclude: ['node_modules', 'dist', '.astro'],
+    exclude: ['node_modules', 'dist', '.astro', 'tests'],
   },
 });
